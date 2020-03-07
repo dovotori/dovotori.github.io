7d22d1b48a17c3458ec10901fce51245ff88df93
dovotori <dratovo@gmail.com>
2020-03-07 19:47:48 +0100

    fix coverage badge

--- jest.config.js
+++ jest.config.js
@@ -21,7 +21,10 @@ module.exports = {
   collectCoverage: true,
 
   // An array of glob patterns indicating a set of files for which coverage information should be collected
-  // collectCoverageFrom: undefined,
+  collectCoverageFrom: [
+    '**/src/**/*.{js,jsx}',
+    '!**/node_modules/**',
+  ],
 
   // The directory where Jest should output its coverage files
   coverageDirectory: 'coverage',
