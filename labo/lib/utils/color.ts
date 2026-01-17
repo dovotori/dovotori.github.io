342b3bf0b40b593c1c3ea69c6eba0939d24db296
dovotori <dovotori@proton.me>
2026-01-17 14:40:24 +0900

    fix hex func

--- labo/lib/utils/color.ts
+++ labo/lib/utils/color.ts
@@ -112,7 +112,7 @@ export const hexToRgb = (hex: string) => {
 
 export const rgbToHex = (r: number, g: number, b: number) => {
   const toHex = (c: number) => {
-    const hex = c.toString(16);
+    const hex = Math.round(c).toString(16);
     return hex.length === 1 ? `0${hex}` : hex;
   };
   return `#${toHex(r)}${toHex(g)}${toHex(b)}`;
